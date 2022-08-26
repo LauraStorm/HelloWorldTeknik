769c635b55402f02292c869361efc8f275270a59
LauraStorm <lbs2rm@hotmail.com>
2022-08-26 08:59:06 +0200

    hello html

---
+++ src/main/java/com/example/springhalloworld/SpringHalloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.springhalloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHalloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHalloWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springhalloworld/SpringHalloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhalloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHalloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
